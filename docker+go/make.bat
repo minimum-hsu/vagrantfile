776c376fa15e0ce7c0e55af36a8d9f22b88eeb4b
Minimum <s5550055@yahoo.com.tw>
2015-11-12 19:17:27 +0800

    Add batch file.

---
+++ docker+go/make.bat
@@ -0,0 +1,7 @@
+mkdir ..\box\docker+go
+vagrant up ubuntu-trusty64 && vagrant package ubuntu-trusty64 --output ../box/docker+go/ubuntu-trusty64.box
+vagrant up ubuntu-vivid64 && vagrant package ubuntu-vivid64 --output ../box/docker+go/ubuntu-vivid64.box
+vagrant up debian-jessie64 && vagrant package debian-jessie64 --output ../box/docker+go/debian-jessie64.box
+vagrant up debian-wheezy64 && vagrant package debian-wheezy64 --output ../box/docker+go/debian-wheezy64.box
+vagrant up centos-7 && vagrant package centos-7 --output ../box/docker+go/centos-7.box
+vagrant up centos-6 && vagrant package centos-6 --output ../box/docker+go/centos-6.box
---
+++ docker/make.bat
@@ -0,0 +1,7 @@
+mkdir ..\box\docker
+vagrant up ubuntu-trusty64 && vagrant package ubuntu-trusty64 --output ../box/docker/ubuntu-trusty64.box
+vagrant up ubuntu-vivid64 && vagrant package ubuntu-vivid64 --output ../box/docker/ubuntu-vivid64.box
+vagrant up debian-jessie64 && vagrant package debian-jessie64 --output ../box/docker/debian-jessie64.box
+vagrant up debian-wheezy64 && vagrant package debian-wheezy64 --output ../box/docker/debian-wheezy64.box
+vagrant up centos-7 && vagrant package centos-7 --output ../box/docker/centos-7.box
+vagrant up centos-6 && vagrant package centos-6 --output ../box/docker/centos-6.box
