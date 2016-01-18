a9d1383d7b1f13ff96de7fbb12db60ca3c9e557f
Minimum <s5550055@yahoo.com.tw>
2016-01-18 16:47:31 +0800

    Update install_go.sh

--- common/install_go.sh
+++ common/install_go.sh
@@ -8,6 +8,6 @@ fi
 curl https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz | tar -C /usr/local -zx
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ${CONFIG}
 echo 'export GOPATH=$HOME/workspace' >> ${CONFIG}
-source /etc/bashrc
+source ${CONFIG}
 mkdir -p /home/vagrant/workspace/src/github.com
 chown -R vagrant: /home/vagrant/workspace
